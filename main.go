f2e19a3cc2aec59a558511da8528c74f23294933
Furkan Çetinkaya <furkancetinkaya0746@gmail.com>
2023-10-19 21:07:50 +0300

    update

--- main.go
+++ main.go
@@ -100,7 +100,7 @@ func main() {
 				return
 			}
 
-			kb.SetKeys(keybd_event.VK_CAPSLOCK, getGear(number))
+			kb.SetKeys(getGear(number))
 			if err := kb.Press(); err != nil {
 				panic(err)
 				return
@@ -111,23 +111,4 @@ func main() {
 			}
 		}
 	}
-
-	//// Select keys to be pressed
-	//kb.SetKeys(keybd_event.VK_A, keybd_event.VK_B)
-	//
-	//// Set shift to be pressed
-	//kb.HasSHIFT(true)
-	//
-	//// Press the selected keys
-	//err = kb.Launching()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//// Or you can use Press and Release
-	//kb.Press()
-	//time.Sleep(10 * time.Millisecond)
-	//kb.Release()
-
-	// Here, the program wilABABl generate "ABAB" as if they were pressed on the keyboard.
 }
